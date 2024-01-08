ab94bd329f990addca4160a2e3b1ca47bbcc6c9c
Gabriele Calarota <gbc@bendingspoons.com>
2024-01-08 16:20:45 +0100

    refactor: update app

--- app/src/main/java/com/onthewifi/casacalarota/spacejet/GameActivity.java
+++ app/src/main/java/com/onthewifi/casacalarota/spacejet/GameActivity.java
@@ -1,7 +1,7 @@
 package com.onthewifi.casacalarota.spacejet;
 
 import android.graphics.Point;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Display;
 
